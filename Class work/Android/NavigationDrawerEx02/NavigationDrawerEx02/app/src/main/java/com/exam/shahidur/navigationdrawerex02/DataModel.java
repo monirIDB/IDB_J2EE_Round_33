045d3fd38b39247b871628dc8034083d677a78b0
Monir <mmzbueta16@gmail.com>
2019-04-15 19:05:23 +0600

    android NavigationDrawerEx02

---
+++ Class work/Android/NavigationDrawerEx02/NavigationDrawerEx02/app/src/main/java/com/exam/shahidur/navigationdrawerex02/DataModel.java
@@ -0,0 +1,17 @@
+package com.exam.shahidur.navigationdrawerex02;
+
+/**
+ * Created by APCL-1 on 10/9/2016.
+ */
+public class DataModel {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
