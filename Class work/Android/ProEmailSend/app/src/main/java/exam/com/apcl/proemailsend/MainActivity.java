a5e16b7067f2fa2428a9e872c01eda6a1ecad8ce
Monir <mmzbueta16@gmail.com>
2019-04-17 19:04:27 +0600

    android proEmailSend

---
+++ Class work/Android/ProEmailSend/app/src/main/java/exam/com/apcl/proemailsend/MainActivity.java
@@ -0,0 +1,16 @@
+package exam.com.apcl.proemailsend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email, sub, body;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
